2d4b6ce522c798b40444d8f6d6797d6ca36e6cb4
Ellen Shapiro <designatednerd@gmail.com>
2020-12-13 20:34:47 -0600

    Initial Commit

---
+++ Kittstagram/KittstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittstagramApp.swift
+//  Kittstagram
+//
+//  Created by Ellen Shapiro on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct KittstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
